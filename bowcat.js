9293614f6ea15087ca4a888be7bd5d7980e8de8d
Ajay MT <ajay.tatachar@gmail.com>
2014-10-08 21:19:35 +0530

    Proper dependency handling.

--- bowcat.js
+++ bowcat.js
@@ -14,7 +14,7 @@ if (opts.help) {
   process.exit(0);
 }
 
-var inputDir = path.resolve(opts._[0]);
+var inputDir = path.resolve(opts._[0]) || '.';
 var outputDir = opts.o || path.join('.', 'build');
 outputDir = path.resolve(outputDir);
 
@@ -24,26 +24,29 @@ pkgs = _.map(pkgs, function (p) {
   return path.join(inputDir, 'bower_components', p);
 });
 
-function concatPackages (packages, outDir) {
-  if (! outDir) outDir = path.join('.', 'build');
+var concatedPkgs = [];
 
-  var files = [];
+function concatPackage (package, outDir) {
+  var bowerJSON = JSON.parse(fs.readFileSync(path.join(package, 'bower.json')));
+  var deps = bowerJSON.dependencies || [];
 
-  _.each(packages, function (package, i, l) {
-    var packageFiles = fs.readdirSync(package);
-
-    files = files.concat(_.map(packageFiles, function (f) {
-      return path.join(package, f);
-    }));
+  _.each(Object.keys(deps), function (pkg, i, l) {
+    var components = package.split(path.sep);
+    var pkgpath = components.slice(components.length - 1).join(path.sep);
+    concatPackage(path.join(pkgpath, pkg));
   });
 
-  var concatJS = '';
-  var concatCSS = '';
+  if (_.contains(concatedPkgs, path.basename(package))) return;
+
+  var files = fs.readdirSync(package);
+  files = _.map(files, function (f) {
+    return path.join(package, f);
+  });
+
+  var concatJS = '', concatCSS = '';
 
   _.each(files, function (filepath, i, l) {
-    if (filepath.indexOf('json') !== -1) return;
-
-    var contents = fs.readFileSync(filepath, { encoding: 'utf-8' }) + '\n';
+    var contents = fs.readFileSync(filepath) + '\n';
 
     if (filepath.indexOf('js') === (filepath.length - 2))
       concatJS += contents;
@@ -51,14 +54,23 @@ function concatPackages (packages, outDir) {
       concatCSS += contents;
   });
 
-  if (concatJS !== '' || concatCSS !== '')
-    fs.mkdirSync(outDir);
-
   if (concatJS !== '')
     fs.writeFileSync(path.join(outDir, 'build.js'), concatJS);
 
   if (concatCSS !== '')
-    fs.writeFileSync(path.join(outDir, 'build.css'), concatJS);
+    fs.writeFileSync(path.join(outDir, 'build.css'), concatCSS);
+
+  concatedPkgs.push(path.basename(package));
+}
+
+function concatPackages (packages, outDir) {
+  if (! outDir) outDir = path.join('.', 'build');
+
+  fs.mkdirSync(outDir);
+
+  _.each(packages, function (package, i, l) {
+    concatPackage(package, outDir);
+  });
 }
 
 module.exports.concatPackages = concatPackages;
